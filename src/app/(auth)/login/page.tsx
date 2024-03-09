5a37fdc5edef10cea2cc10def03a2c38c9bcda76
= <=>
2024-03-09 11:52:01 +0530

    auth added to layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -37,7 +37,7 @@ const Login = (props: LoginProps) => {
             email: values.email,
             password: values.password,
             redirect: true,
-            callbackUrl: "/dashboard",
+            callbackUrl: "/employees",
         });
     };
 
