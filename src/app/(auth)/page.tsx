3bca18dee4fa1880cdcfe1a97ab78bee16c40170
= <=>
2024-03-09 23:40:34 +0530

    root route fix

---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,16 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+import { authOptions } from "@/lib/auth-options";
+
+export default async function Auth() {
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+        redirect("/login");
+    } else {
+        redirect("/employees");
+    }
+
+}
