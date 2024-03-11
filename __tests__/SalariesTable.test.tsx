9338e0464f5b02650c4fab7475bc10c446542e9f
= <=>
2024-03-11 12:12:13 +0530

    code clean up

--- __tests__/SalariesTable.test.tsx
+++ __tests__/SalariesTable.test.tsx
@@ -1,22 +1,4 @@
-import { screen, render } from '@testing-library/react'
-
-import SalariesTable from '@/app/(dashboard)/salaries/_components/SalariesTable'
-import { Employee } from '@prisma/client'
-
 import * as utils from '../src/lib/utils';
-
-
-const employees: Employee[] = [
-    {
-        id: '1',
-        name: 'Kewin',
-        joiningDate: new Date(),
-        basicSalary: '1000',
-        salaryAllowance: '100',
-        processingDate: new Date(),
-        isEndOfService: false,
-    }
-]
 
 it('should add 10 to basic and salary allowance', () => {
     expect(utils.addToSalary('1000', '100', 10, 0)).toEqual('1110')
